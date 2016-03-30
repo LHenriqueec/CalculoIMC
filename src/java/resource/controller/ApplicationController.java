81815285bfe5fc001408a192b34360240ad38816
LuizS <luizsnip@gmail.com>
2016-03-30 15:53:25 -0300

    Desenvolvimento do Controller

--- src/java/resource/controller/ApplicationController.java
+++ src/java/resource/controller/ApplicationController.java
@@ -1,6 +1,5 @@
 package controller;
 
-
 import domain.Domain;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
@@ -11,9 +10,9 @@ import javafx.util.StringConverter;
 import javafx.util.converter.NumberStringConverter;
 
 public class ApplicationController {
-	
+
 	private Domain domain;
-	
+
 	@FXML
 	private TextField txtAltura;
 	@FXML
@@ -22,33 +21,59 @@ public class ApplicationController {
 	private Label lblImc;
 	@FXML
 	private Label lblStatus;
-	
+
 	@FXML
 	private void initialize() {
 		domain = new Domain();
-		
+
 		StringConverter<Number> converter = new NumberStringConverter();
 		Bindings.bindBidirectional(txtAltura.textProperty(), domain.alturaProperty(), converter);
 		Bindings.bindBidirectional(txtPeso.textProperty(), domain.pesoProperty(), converter);
 	}
-	
+
 	@FXML
 	private void handleCalc() {
-		
+		lblImc.setText(String.format("%.2f", calcImc()));
+		status();
 	}
-	
+
 	@FXML
 	private void handleClear() {
-		domain.setAltura(0);
-		domain.setPeso(0);
+		txtAltura.setText("0");
+		txtPeso.setText("0");
+		lblImc.setText("0");
 	}
-	
+
 	@FXML
 	private void handleClose() {
 		Platform.exit();
 	}
-	
+
 	private double calcImc() {
-		return 0;
+		return (domain.getAltura() == 0 || domain.getPeso() == 0) ? 0
+				: domain.getPeso() / (domain.getAltura() * domain.getAltura());
+	}
+
+	private void status() {
+		if (calcImc() < 20.7) {
+			lblStatus.setText("ABAIXO DO PESO");
+		} else {
+
+			if (calcImc() < 26.5) {
+				lblStatus.setText("PESO NORMAL");
+			} else {
+
+				if (calcImc() < 27.9) {
+					lblStatus.setText("MARGINALMENTE ACIMA DO PESO");
+				} else {
+
+					if (calcImc() < 31.2) {
+						lblStatus.setText("ACIMA DO PESO IDEAL");
+					} else {
+						lblStatus.setText("OBESO");
+					}
+				}
+			}
+		}
 	}
 }
